7c48b0af1fc084598f0b4050b4cfbc17c57203f6
Rockets <vkomolov.ua@gmail.com>
2025-09-10 13:50:57 +0300

    to upgrade tsconfig.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,46 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js';
+import globals from 'globals';   // installing module
+import nextPlugin from '@next/eslint-plugin-next';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  js.configs.recommended,          // basic rules ESLint
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.browser,      // browser globals: window, document, console …
+        ...globals.node,         // node: process, Buffer, console …
+      },
+    },
+    plugins: {
+      '@next/next': nextPlugin,
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules, // recommended rules Next.js
+      ...tsPlugin.configs.recommended.rules,   // recommended rules TS
+      //! further rules can be here...
+    },
+  },
+  {
+    //! ignoring paths which are not required to be linted...
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'next-env.d.ts',
+      '.vscode/**',
+      'docs/**',
+      'scripts/**',
+      '*.config.{js,mjs,ts}',
+    ],
+  },
 ];
-
-export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	//eslint: { ignoreDuringBuilds: true }, //not to run lint when building
 	/* config options here */
 };
 
